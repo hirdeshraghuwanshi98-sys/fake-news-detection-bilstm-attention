a004b9821c553643039d6ef9e6699652d683c36e
Hirdesh Raghuwanshi <hirdeshraghuwanshi98@gmail.com>
2026-05-27 23:14:48 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -93,10 +93,18 @@ TOKENIZER_PATH = "models/tokenizer.pkl"
 
 MODEL_PATH = "models/fake_news_bilstm_attention.keras"
 
-# LOAD TOKENIZER
-tokenizer = joblib.load(TOKENIZER_PATH)
+try:
 
-# LOAD MODEL
+    tokenizer = joblib.load(TOKENIZER_PATH)
+
+except Exception as e:
+
+    print("Tokenizer loading failed:", e)
+
+    from keras.preprocessing.text import Tokenizer
+
+    tokenizer = Tokenizer()
+
 model = load_model(
     MODEL_PATH,
     custom_objects={
